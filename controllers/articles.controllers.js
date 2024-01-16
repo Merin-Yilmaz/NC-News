417388575dfce881fd74ba641ceb01dddb654a16
Merin Yilmaz <merin.yilmaz@gmail.com>
2024-01-16 16:02:01 +0000

    upload completed task 6 CORE: GET /api/articles/:article_id/comments

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,24 +1,38 @@
 // controller
 
-const { fetchArticleById, fetchAllArticles } = require('../models/articles.models')
+const {
+  fetchArticleById,
+  fetchAllArticles,
+  fetchCommentsByArticleId,
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
-    const { article_id } = req.params;
+  const { article_id } = req.params;
 
-    fetchArticleById(article_id)
+  fetchArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
     })
     .catch(next);
-  };
-  
+};
 
 exports.getAllArticles = (req, res, next) => {
-
-    fetchAllArticles().then((articles) => {
-        res.status(200).send({ articles })
+  fetchAllArticles()
+    .then((articles) => {
+      res.status(200).send({ articles });
     })
     .catch((err) => {
-        next(err);
+      next(err);
     });
-}; 
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+    const { article_id } = req.params;
+    fetchCommentsByArticleId(article_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
